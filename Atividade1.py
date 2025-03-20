841b06ef0cb88a1b3a9eb7775951b85166b0c88a
GabrielJ10 <gabrieljust123@gmal.com>
2025-03-19 21:56:01 -0300

    atividade 3

--- Atividade1.py
+++ Atividade1.py
@@ -1,4 +1,9 @@
-def cadastrar_usuario(nome):
-    print("Cadastro realizado para:", nome)
-    print("Cadastro realizado para:", nome)  
-cadastrar_usuario("Ana")
+def ler_arquivo(nome_arquivo):
+    try:
+        arquivo = open(nome_arquivo, "r")
+        conteudo = arquivo.read()
+        arquivo.close()
+        return conteudo
+    except:
+        print("Erro ao ler arquivo") 
+print(ler_arquivo("dados.txt"))
